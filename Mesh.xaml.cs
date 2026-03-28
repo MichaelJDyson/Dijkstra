4b8ac02c5db5584e4f2016704012eed3da2721d6
Mike Dyson <mike@dysonhome.com>
2026-03-28 16:11:11 +0000

    Did commit

--- Mesh.xaml.cs
+++ Mesh.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Dijkstra;
-
+// One agreed change
 public partial class Mesh : UserControl
 {
     Label[,] cell; // do not allocate here
